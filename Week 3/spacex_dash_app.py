2c8213c062c915483a3ab30f5b0f762a3c212d42
Nizaaf Dabir <110449627+NizaafDabir@users.noreply.github.com>
2023-07-31 10:26:27 +0530

    Dashboard using Plotly dash

--- Week 3/spacex_dash_app.py
+++ Week 3/spacex_dash_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, StateA
 import plotly.graph_objects as go
 import plotly.express as px
 from dash import no_update
